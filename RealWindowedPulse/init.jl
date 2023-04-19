1e5fd2d0c1c8b7df6dda3be8f814b8fe83ad9636
Kyle Sherbert <kyles@Harpers-Mac-mini.local>
2023-04-19 16:30:15 -0400

    Initialize workspace

---
+++ RealWindowedPulse/init.jl
@@ -0,0 +1,46 @@
+#= Read in parameters and initialize a job directory.
+
+# Usage
+```julia init.jl matrix T W r m ```
+
+# Parameters
+- matrix: filename (without path or extension) of matrix
+- T: Pulse duration in ns
+- W: Number of windows in the square pulse.
+- r: Number of Trotter steps.
+- m: Size of Hilbert space for each qubit.
+
+For convenience,
+    omitting m defaults to 2,
+    omitting r defaults to 20T, and
+    omitting W defaults to 5T.
+You can't omit earlier arguments without omitting later arguments.
+
+=#
+
+shift!(array, default=nothing) = length(array) > 0 ? popfirst!(array) : default
+
+# CONSTRUCT jobdir AND ENSURE IT IS AVAILABLE
+jobdir = join(ARGS, "_")
+isdir(jobdir) && error("Some data already exists! Delete or rename directory '$jobdir'")
+
+# PARSE ARGUMENTS AS SETTINGS
+import RealWindowedPulse as JOB
+
+matrix = shift!(ARGS)
+T = parse(Float64, shift!(ARGS))
+W = shift!(ARGS, nothing)
+r = shift!(ARGS, nothing)
+m = shift!(ARGS, nothing)
+
+JOB.modify_settings(
+    matrixfile="HPCJobs/matrix/$matrix.npy",
+    T=T,
+    W= !isnothing(W) ? parse(Int, W) : round(Int, 5T),
+    r= !isnothing(r) ? parse(Int, r) : round(Int,20T),
+    m= !isnothing(m) ? parse(Int, m) : 2,
+)
+mkpath(jobdir)
+JOB.save_settings(jobdir)
+
+println(jobdir)     # EXPORTS `jobdir` TO THE CALLING BASH SCRIPT
---
+++ matrix/LEGEND.md
@@ -0,0 +1,26 @@
+# Legend
+
+### Hydrogen Chains and Cycles
+Matrices with filename "{c?}H{n}{E|S}{J|B|P}{m?}.npy" correspond to hydrogen toy models.
+
+The label "H{N}" indicates a line of N equally-spaced hydrogens,
+    while  "cH{N}" indicates a ring.
+
+The next letter indicates whether the geometry is near equilibrium (E) or stretched (S).
+For stretched geometries, the distance between nearest neighbors is 3.0Å.
+For equilibrium geometries, the geomtries are NOT optimized, so they are only _close_ to equilibrium.
+The exact seaparations are given in the section below.
+
+The next letter indicates the qubit mapping used:
+    Jordan-Wigner (J), Bravyi-Kitaev (B) or parity (P).
+The presence of an m after the mapping indicates whether a two-qubit tapering was applied
+    to account for conservation of particles in each spin sector.
+
+#### Equilibrium Geometries
+- H2: 0.75Å
+- H4: 0.90Å
+- H6: 0.95Å
+- H8: 0.95Å
+- cH4: 1.25Å
+- cH6: 1.00Å
+- cH8: 1.05Å
